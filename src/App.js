3bc55a7ab5f09e0b1d4f6dfa987fab270234d1ef
shreyareddy913 <64725160+shreyareddy913@users.noreply.github.com>
2020-08-27 13:32:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import OptionSelection from './components/OptionSelection';
+import TypingTextComponent from './components/TypingTextComponent';
+import axios from 'axios';
 
 function App() {
+  const [selectedObject, updateSelectedObject] = useState({});
+  const [modelPrediction, setModelPrediction] = useState(null);
+  const [isPredictionLoading, setPredictionLoading] = useState(false);
+  const [isRequestFailed, setRequestFailed] = useState(null);
+  const setOptionInObject = (itemKey, consoleOption) => {
+    const tempSelectedObject = selectedObject;
+    tempSelectedObject[itemKey] = consoleOption;
+    updateSelectedObject(tempSelectedObject);
+  }
+  const handleInputSubmission = () => {
+    if(selectedObject && Object.keys(selectedObject).length === 7) {
+      setPredictionLoading(true)
+      setModelPrediction(null)
+      setRequestFailed(null)
+      axios.post(process.env.REACT_APP_HEROKU_SERVER_URL, selectedObject)
+      .then(function (response) {
+        setPredictionLoading(false)
+        setModelPrediction(response.data.result)
+      })
+      .catch(function (error) {
+        setPredictionLoading(false)
+        setRequestFailed("Some error ocurred while fetching prediction")
+      });
+    } else {
+      setRequestFailed("Please select all fields before submitting request")
+    }
+  }
+  const dropDownKeys = ["CONSOLE","YEAR","CATEGORY","PUBLISHER","RATING","CRITICS_POINTS","USER_POINTS"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-dark justify-content-center">
+        <center><h3 className="title"><span role="img" aria-label="game">👾</span> Video games sales prediction</h3></center>
+      </nav>
+      <TypingTextComponent />
+      <div className="container d-flex justify-content-center align-items-center flex-column">
+        <div className="container-grid">
+        {
+          dropDownKeys.map((item,index)=>{
+            return <OptionSelection itemKey={item} setOptionInObject={setOptionInObject} key={index}/> 
+          })
+        }
+        </div>
+        <div className="submit-button-container">
+          <button className="btn btn-grad" onClick={() => handleInputSubmission()}>
+          {(() => {
+            if(isPredictionLoading) {
+              return(
+                <div className="spinner-border text-light" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              )
+            } else {
+              return(
+                <span>Submit</span>
+              )  
+            }
+          })()}
+          </button>
+        </div>
+        <div className="mt-4">
+          {modelPrediction ? 
+          <span className="h2">Predicted Sales in Millions: {modelPrediction.toFixed(2)}</span> 
+          : 
+          null }
+          {isRequestFailed ? 
+          <span className="h4">{isRequestFailed}</span> 
+          : 
+          null }
+        </div>
+      </div>
     </div>
   );
 }
